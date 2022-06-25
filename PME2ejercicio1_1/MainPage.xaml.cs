5034d351c0315210dd6c267aa49fd8ac56c1b842
yesman4-dev <56881149+yesman4-dev@users.noreply.github.com>
2022-06-24 20:02:47 -0600

    Agregar archivos de proyecto.

---
+++ PME2ejercicio1_1/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Ejercici11;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PME2ejercicio1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double n1, n2;
+
+            n1 = double.Parse(num1.Text);
+            n2 = double.Parse(num2.Text);
+
+            resultado = n1 + n2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double n1, n2;
+
+            n1 = double.Parse(num1.Text);
+            n2 = double.Parse(num2.Text);
+
+            resultado = n1 - n2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double n1, n2;
+
+            n1 = double.Parse(num1.Text);
+            n2 = double.Parse(num2.Text);
+
+            resultado = n1 * n2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double n1, n2;
+
+            n1 = double.Parse(num1.Text);
+            n2 = double.Parse(num2.Text);
+
+            resultado = n1 / n2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+    }
+}
